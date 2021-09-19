43b9011d04c7775dae4f66bd3f89d1a954ec6592
thallard <thallard@student.42lyon.fr>
2021-09-19 23:41:25 +0200

    Push

---
+++ daemon/cmake-build-debug/CMakeFiles/taskmaster_daemon.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/hosra/Desktop/42/taskmaster/daemon/srcs/Execution.cpp" "/Users/hosra/Desktop/42/taskmaster/daemon/cmake-build-debug/CMakeFiles/taskmaster_daemon.dir/srcs/Execution.cpp.o"
+  "/Users/hosra/Desktop/42/taskmaster/daemon/srcs/Service.cpp" "/Users/hosra/Desktop/42/taskmaster/daemon/cmake-build-debug/CMakeFiles/taskmaster_daemon.dir/srcs/Service.cpp.o"
+  "/Users/hosra/Desktop/42/taskmaster/daemon/srcs/TunnelTCP.cpp" "/Users/hosra/Desktop/42/taskmaster/daemon/cmake-build-debug/CMakeFiles/taskmaster_daemon.dir/srcs/TunnelTCP.cpp.o"
+  "/Users/hosra/Desktop/42/taskmaster/daemon/srcs/main.cpp" "/Users/hosra/Desktop/42/taskmaster/daemon/cmake-build-debug/CMakeFiles/taskmaster_daemon.dir/srcs/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././includes"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ daemon/cmake-build-debug/CMakeFiles/taskmaster_daemon.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/taskmaster_daemon.dir/srcs/Execution.cpp.o"
+  "CMakeFiles/taskmaster_daemon.dir/srcs/Service.cpp.o"
+  "CMakeFiles/taskmaster_daemon.dir/srcs/TunnelTCP.cpp.o"
+  "CMakeFiles/taskmaster_daemon.dir/srcs/main.cpp.o"
+  "bin/taskmaster_daemon"
+  "bin/taskmaster_daemon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/taskmaster_daemon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
